fe2584c94435a3b35c0460e67903dfbda6c05c96
yokikoshimizu <heppoko.yokiki@gmail.com>
2022-03-10 20:56:20 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
 
 function App() {
+  const [count, setCount ] = useState(0);
+  const [text, setText ] = useState('');
+  const [showtext, setShowtext ] = useState('');
+  
+  function buttonConsole() {
+    console.log("Hello World")
+  }
+  
+  const buttonAlert = () => {
+    window.alert('Hello World')
+  }
+
+
+    function increment() {
+      setCount(count + 1);
+      console.log(count);
+    }
+    const decrement = () => {
+      setCount(count - 1);
+      console.log(count);
+    }
+    const countReset = () => {
+      setCount(0);
+      console.log(count);
+    }
+
+  const handleClickOutPut = () => {
+    setShowtext(text)
+  };
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+
+   
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React 1
-        </a>
-      </header>
+    <div>
+      <div>
+        <button onClick={increment}>いいね</button>
+        <button onClick={decrement}>よくないね</button>
+        <button onClick={countReset}>リセット!</button>
+        {count}
+      </div>
+
+      <div>
+        <button onClick={handleClickOutPut}>button</button>
+        <input type="text" value={text} onChange={handleChange} />
+        {showtext}
+      </div>
+      <div>
+        {text ?  "" : <p style={{color:"red"}}>何か文字を入力してください。</p> }
+      </div>
+      <div>
+        <button onClick={buttonConsole}>Hello World</button>
+        <button onClick={buttonAlert}>Hello World2</button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
